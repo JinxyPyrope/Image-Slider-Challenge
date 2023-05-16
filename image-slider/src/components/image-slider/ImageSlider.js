6449bd31410766eecdf3c02487fd0b99b617d8f2
Nhoj <nhojplummer@yahoo.com>
2023-05-16 15:24:59 -0400

    Will continue this challenge tomorrow

--- image-slider/src/components/image-slider/ImageSlider.js
+++ image-slider/src/components/image-slider/ImageSlider.js
@@ -2,11 +2,9 @@ import React from "react"
 import { image01, image02, image03, image04 } from "./import"
 
 function ImageSlider() {
-  return (
-    <div className="slideshow-container">
-   
-    </div>
-  )
+  const dataSlider = []
+
+  return <div className="slide-container"></div>
 }
 
 export default ImageSlider
