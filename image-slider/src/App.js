faa76c81a518816dc05b93374d1dc06473495d79
Nhoj <nhojplummer@yahoo.com>
2023-05-17 13:23:23 -0400

    Starting from square one

--- image-slider/src/App.js
+++ image-slider/src/App.js
@@ -1,13 +1,13 @@
 import "./App.css"
 import { ImageSlider } from "./components"
-import { image01, image02, image03, image04 } from "./components/image-slider/import"
+
 
 function App() {
-  const images = [{ id: 1, url: { image01 } }]
+
 
   return (
     <div className="App">
-      <ImageSlider images={images}/>
+      <ImageSlider />
     </div>
   )
 }
--- image-slider/src/components/image-slider/ImageSlider.js
+++ image-slider/src/components/image-slider/ImageSlider.js
@@ -1,13 +1,10 @@
 import React from "react"
 
 function ImageSlider() {
-  // const images = [{ url: {} }]
+
   return (
     <div className="slides-container">
-      <div className="mySlides">
-        <div className="text">Caption Text</div>
-        <img src="../../assets/image1.jpg" alt="test" />
-      </div>
+     
     </div>
   )
 }
