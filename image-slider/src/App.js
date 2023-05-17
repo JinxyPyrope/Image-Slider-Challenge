ca84aa51c7954eb6a261c3c9962de3660c6f2243
Nhoj <nhojplummer@yahoo.com>
2023-05-17 12:53:07 -0400

    Setting up a new idea for the challenge

--- image-slider/src/App.js
+++ image-slider/src/App.js
@@ -2,6 +2,8 @@ import "./App.css"
 import { ImageSlider } from "./components"
 
 function App() {
+  const slides = [{ url: "../../assets/image1.jpg" }]
+
   return (
     <div className="App">
       <ImageSlider />
--- image-slider/src/components/image-slider/ImageSlider.js
+++ image-slider/src/components/image-slider/ImageSlider.js
@@ -1,8 +1,7 @@
 import React from "react"
-import { image01, image02, image03, image04 } from "./import"
+
 
 function ImageSlider() {
-  const dataSlider = []
 
   return <div className="slide-container"></div>
 }
