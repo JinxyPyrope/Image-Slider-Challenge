7f41ad814756ce56537976094c7481e7187bdc34
Nhoj <nhojplummer@yahoo.com>
2023-05-17 13:16:04 -0400

    Setting up a new idea again

--- image-slider/src/components/image-slider/ImageSlider.js
+++ image-slider/src/components/image-slider/ImageSlider.js
@@ -1,9 +1,15 @@
 import React from "react"
 
-
 function ImageSlider() {
-
-  return <div className="slide-container"></div>
+  // const images = [{ url: {} }]
+  return (
+    <div className="slides-container">
+      <div className="mySlides">
+        <div className="text">Caption Text</div>
+        <img src="../../assets/image1.jpg" alt="test" />
+      </div>
+    </div>
+  )
 }
 
 export default ImageSlider
