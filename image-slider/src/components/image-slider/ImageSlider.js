f0ec5415a1404ad6e269e74942ed5397cdbef1cd
Nhoj <nhojplummer@yahoo.com>
2023-05-17 13:54:27 -0400

    Finally controlled in resizing image

--- image-slider/src/components/image-slider/ImageSlider.js
+++ image-slider/src/components/image-slider/ImageSlider.js
@@ -1,10 +1,11 @@
 import React from "react"
+import "./ImageSlider.css"
 import { image01 } from "./import"
 
 function ImageSlider() {
   return (
     <div className="slides-container">
-      <img src={image01} alt="test" />
+      <img src={image01} alt="test" max-width="100%"/>
     </div>
   )
 }
