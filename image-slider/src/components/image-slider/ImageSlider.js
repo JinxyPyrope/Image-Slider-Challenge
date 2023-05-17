77d8558fb7c403cac0dcc8756951e294fc049bc3
Nhoj <nhojplummer@yahoo.com>
2023-05-17 13:39:43 -0400

    Finally got an image in now I must try to get the css done

--- image-slider/src/components/image-slider/ImageSlider.js
+++ image-slider/src/components/image-slider/ImageSlider.js
@@ -1,10 +1,10 @@
 import React from "react"
+import { image01 } from "./import"
 
 function ImageSlider() {
-
   return (
     <div className="slides-container">
-     
+      <img src={image01} alt="test" />
     </div>
   )
 }
