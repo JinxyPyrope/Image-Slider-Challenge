1bf1dbd1a110521c8acdcbc2232a1192f12a2802
Nhoj <nhojplummer@yahoo.com>
2023-05-18 12:39:25 -0400

    Need to reset on the design

--- image-slider/src/components/image-slider/ImageSlider.js
+++ image-slider/src/components/image-slider/ImageSlider.js
@@ -3,34 +3,7 @@ import "./ImageSlider.css"
 import { image01, image02, image03, image04 } from "./import"
 
 function ImageSlider() {
-  return (
-    <div className="slides-container">
-      <div className="slide">
-        <img src={image01} alt="test" />
-        <div class="text">Caption Text</div>
-      </div>
-      <div className="slide">
-        <img src={image02} alt="test" />
-        <div class="text">Caption Text</div>
-      </div>
-      <div className="slide">
-        <img src={image03} alt="test" />
-        <div class="text">Caption Text</div>
-      </div>
-      <div className="slide">
-        <img src={image04} alt="test" />
-        <div class="text">Caption Text</div>
-      </div>
-
-      <a className="prev">&#10094;</a>
-      <a className="next">&#10095;</a>
-      <div className="slides-container__dot-section">
-        <span className="dot"></span>
-        <span className="dot"></span>
-        <span className="dot"></span>
-      </div>
-    </div>
-  )
+  return <div className="slides-container"></div>
 }
 
 export default ImageSlider
