1eda8af610d4c237c06b7194094c0c3e6dd83001
Nhoj <nhojplummer@yahoo.com>
2023-05-19 15:30:52 -0400

    Images imported from an array

--- image-slider/src/components/image-slider/import.js
+++ image-slider/src/components/image-slider/import.js
@@ -1,5 +1,13 @@
 import image1 from "../../assets/image1.jpg"
+import image2 from "../../assets/image2.jpg"
+import image3 from "../../assets/image3.jpg"
+import image4 from "../../assets/image4.jpg"
 
-const data = [{ id: 1, url: image1 }]
+const data = [
+  { id: 1, url: image1 },
+  { id: 2, url: image2 },
+  { id: 3, url: image3 },
+  { id: 4, url: image4 }
+]
 
 export default data
