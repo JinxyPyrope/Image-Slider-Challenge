7293261558cf799da1e1383dd3d922629951ceb3
Nhoj <nhojplummer@yahoo.com>
2023-05-19 14:03:05 -0400

    Mixing up ideas

--- image-slider/src/components/image-slider/ImageSlider.js
+++ image-slider/src/components/image-slider/ImageSlider.js
@@ -1,9 +1,23 @@
 import React from "react"
 import "./ImageSlider.css"
-import { image01, image02, image03, image04 } from "./import"
+import image1 from "../../assets/image1.jpg"
+// import dataSlider from "./dataSlider"
 
 function ImageSlider() {
-  return <div className="slides-container"></div>
+  return (
+    <div className="slides-container">
+      <div>
+        <img alt="test"/>
+      </div>
+      {/* {dataSlider.map(obj => {
+        return (
+          <div>
+            <img src={require(`../../assets/${obj.url}`)} alt="test" />
+          </div>
+        )
+      })} */}
+    </div>
+  )
 }
 
 export default ImageSlider
---
+++ image-slider/src/components/image-slider/dataSlider.js
@@ -0,0 +1,10 @@
+// import image01 from "../../assets/image1.jpg"
+
+const dataSlider = [
+  {
+    id: 1,
+    url: "../../assets/image1.jpg"
+  }
+]
+
+export default dataSlider
