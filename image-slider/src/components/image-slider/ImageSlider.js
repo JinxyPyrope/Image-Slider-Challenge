074914a67e05e38a0838870c5dcfe2974bd77288
Nhoj <nhojplummer@yahoo.com>
2023-05-21 12:52:51 -0400

    Making progress gonnna useState it

--- image-slider/src/components/image-slider/ImageSlider.js
+++ image-slider/src/components/image-slider/ImageSlider.js
@@ -1,10 +1,13 @@
-import React from "react"
+import React, { useState } from "react"
 import "./ImageSlider.css"
 // import image1 from "./import"
 // import image1 from "../../assets/image1.jpg"
 import data from "./import"
 
 function ImageSlider() {
+  const [current, setCurrent] = useState(0)
+  const length = data.length
+
   return (
     <div className="slides-container">
       {data.map((image, index) => {
